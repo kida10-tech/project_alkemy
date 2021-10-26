8c5357106b038e75f074507018b123d4df1d078e
Diego Martinez Giardini <diego.martinez.giardini@gmail.com>
2021-10-26 19:39:53 -0300

    Entityupdate

--- src/main/java/com/alkemy/icons/icons/entity/CountryEntity.java
+++ src/main/java/com/alkemy/icons/icons/entity/CountryEntity.java
@@ -28,19 +28,9 @@ public class CountryEntity {
     @Column(name = "continent_id", nullable = false)
     private Long continentId;
 
-    @ManyToMany(cascade = {CascadeType.PERSIST,CascadeType.MERGE})
+    @ManyToMany(cascade = {CascadeType.PERSIST,CascadeType.MERGE}) //genera tabla intermedia para relacionar tablas
     @JoinTable(name = "icon_country",
     joinColumns = @JoinColumn(name = "country_id"),
     inverseJoinColumns = @JoinColumn(name = "icon_id"))
     private Set<IconEntity> icons = new HashSet<>();
-
-    @Override
-    public boolean equals(Object obj) {
-        if(obj == null)
-            return false;
-        if(getClass() != obj.getClass())
-            return false;
-        final CountryEntity other = (CountryEntity) obj;
-        return other.id.equals(this.id);
-    }
 }
